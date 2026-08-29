0b85acb3759a976abd3081167aa2d750810c127c
axiom <dandoutelcel@gmail.com>
2026-08-29 16:52:48 -0300

    Add Firebase configuration to config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDIYO_mpJyyS8yydYnAuLvXOKKvhCyGb_A",
+  authDomain: "rpgform-4d5d2.firebaseapp.com",
+  databaseURL: "https://rpgform-4d5d2-default-rtdb.firebaseio.com",
+  projectId: "rpgform-4d5d2",
+  storageBucket: "rpgform-4d5d2.firebasestorage.app",
+  messagingSenderId: "677095520412",
+  appId: "1:677095520412:web:3ccd16067b5ebdf3279559",
+  measurementId: "G-HYCWGP5KGM"
+};
